20d7b0f344833abab351264cace65b23d2a34a34
David <d.lepaux@gmail.com>
2014-07-17 12:26:21 +0200

    Migration Menus pivot

--- app/database/migrations/2014_05_15_121457_create_menu_article_table.php
+++ app/database/migrations/2014_05_15_121457_create_menu_article_table.php
@@ -3,7 +3,7 @@
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateMenuArticlesTable extends Migration {
+class CreateMenuArticleTable extends Migration {
 
 	/**
 	 * Run the migrations.
