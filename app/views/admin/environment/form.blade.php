ff214339bd9b5d10928584620d50bc94f13d07dc
David <d.lepaux@gmail.com>
2014-10-08 17:16:58 +0200

    Update form.blade.php

--- app/views/admin/environment/form.blade.php
+++ app/views/admin/environment/form.blade.php
@@ -3,7 +3,7 @@
     <div class="checkbox checkbox-button">
       <label>
         <input type="checkbox" name="{{$lang['id']}}" value="{{$lang['id']}}"{{( $lang['enable'] == 1 ? ' checked="checked" class="enable"' : ' class="disable"' )}}>
-        <span style="display:inline-block; min-width:40px; text-align:center;">
+        <span>
             <img height="19px" src="{{$lang['flag']}}" alt="{{$lang['id']}}"/>
             {{$lang['name_locale']}} ({{$lang['name_en']}})
         </span>
