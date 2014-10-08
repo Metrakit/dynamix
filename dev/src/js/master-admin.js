d8f889943fd354ac16325ead3437043ca7db8691
David <d.lepaux@gmail.com>
2014-10-08 11:39:12 +0200

    Create master-admin.js

---
+++ dev/src/js/master-admin.js
@@ -0,0 +1,30 @@
+//Object MasterAdmin
+var MasterAdmin = function (){
+	this.start = function (){
+    initListener();
+	}
+	
+	var initListener = function () {
+	  //checkbox button states
+	  $('body').on('click', '.checkbox.checkbox-button .enable', function () 
+    {
+      if ($(this).hasClass('disabled')) {
+        $(this).removeClass('disabled');  
+      } else {
+        $(this).addClass('disabled');
+      }
+    });
+    
+    $('body').on('click', '.checkbox.checkbox-button .disable', function () 
+    {
+      if ($(this).hasClass('enabled')) {
+        $(this).removeClass('enabled');  
+      } else {
+        $(this).addClass('enabled');
+      }
+    });
+	}
+}
+
+
+var masterAdminClass = new MasterAdmin();
