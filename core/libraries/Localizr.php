2ab7478dddc006672369c2980b55a34e61645590
David Lepaux <d.lepaux@gmail.com>
2014-11-13 00:41:29 +0100

    Fix bug composer install

--- core/libraries/Localizr.php
+++ core/libraries/Localizr.php
@@ -9,39 +9,49 @@ class Localizr
 	//get the locale id in url, if exist is it setted else default locale is setted
 	public static function initLocale () {
 		$locale = Request::segment(1);
-		Log::info(gettype($locale));
-		//Test if segment 1 isnt here, and if is valid
-		if(!in_array($locale, Cachr::getCache('DB_LocaleFrontEnable'))){
-			if (Request::is('/') ) {
-				if ( Session::has('lang') ) {
+		$db_is_ok = false;
+		try {
+			$db_is_ok = DB::connection();
+		} catch (Exception $e) {
+			Log::info($e);
+		}
+		
+		if ($db_is_ok) {
+			//Test if segment 1 isnt here, and if is valid
+			if(!in_array($locale, Cachr::getCache('DB_LocaleFrontEnable'))){
+				if (Request::is('/') ) {
+					if ( Session::has('lang') ) {
+						$locale = Session::get('lang');
+					} else {
+						$locale = Config::get('app.locale');
+					}
+					Session::put('lang',$locale);
+					App::setLocale($locale);
+					Log::info('locale : '.$locale);
+					return null;
+				} 
+
+				if ( Session::has('lang') ) {//For return visit
 					$locale = Session::get('lang');
 				} else {
-					$locale = Config::get('app.locale');
-				}
-				Session::put('lang',$locale);
-				App::setLocale($locale);
-				Log::info('locale : '.$locale);
-				return null;
-			} 
-
-			if ( Session::has('lang') ) {//For return visit
-				$locale = Session::get('lang');
-			} else {
-				//if not, search a lang in server datas and if is valid
-				$locale = $this->detectLocale();			
-				//if no valid lang is in server set default
-				if($locale===null) {
-					Session::put('lang',Config::get('app.locale'));
+					//if not, search a lang in server datas and if is valid
+					$locale = $this->detectLocale();			
+					//if no valid lang is in server set default
+					if($locale===null) {
+						Session::put('lang',Config::get('app.locale'));
+					}
 				}
 			}
-		}
 
-		if ( in_array($locale, Cachr::getCache('DB_LocaleFrontEnable')) && $locale != Session::get('lang') ) {
-			Session::put('translate_request',1);
+			if ( in_array($locale, Cachr::getCache('DB_LocaleFrontEnable')) && $locale != Session::get('lang') ) {
+				Session::put('translate_request',1);
+			}
+			Session::put('lang', $locale);
+			App::setLocale($locale);
+			Log::info('locale : '.$locale);
+		} else {
+			$locale = null;
 		}
-		Session::put('lang', $locale);
-		App::setLocale($locale);
-		Log::info('locale : '.$locale);
 		return $locale;
 	}
 
