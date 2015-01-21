538e35fcd1829f5da19b96deb071bd106a6d0c79
CHOISY Tommy <choisy.tommy@gmail.com>
2015-01-21 22:56:27 +0100

    create migration for task part

---
+++ app/database/migrations/2015_01_18_184423_auth_task.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AuthTask extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create the `label_task` table
+		Schema::create('auth_task', function(Blueprint $table)
+		{
+			$table->engine = 'InnoDB';
+			$table->increments('id')->unsigned();
+			
+			$table->integer('tasks_id')->unsigned()->index();
+			$table->foreign('tasks_id')->references('id')->on('tasks')->onDelete('cascade');
+
+			$table->integer('auth_id')->unsigned()->index();
+			$table->foreign('auth_id')->references('id')->on('auths')->onDelete('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		//
+	}
+
+}
