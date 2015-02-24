729ba3df5c1cbaddfb55288a64645395821c125a
David Lepaux <d.lepaux@gmail.com>
2015-02-24 08:29:18 +0100

    videos migrations

---
+++ app/database/migrations/2014_04_30_105330_create_videos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVideosTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		// Create the `Posts` table
+		Schema::create('videos', function(Blueprint $table)
+		{
+            $table->engine = 'InnoDB';
+			$table->increments('id')->unsigned();
+			
+			$table->string('code');
+
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		// Delete the `videos` table
+		Schema::drop('videos');
+	}
+
+}
