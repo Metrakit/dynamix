84c929d949cfa6c53d9f86e168b09cd7cf7d5d7c
David Lepaux <d.lepaux@gmail.com>
2015-02-27 00:50:05 +0100

    update

---
+++ app/config/admin.php
@@ -0,0 +1,24 @@
+<?php
+
+return array(
+
+	/*
+	|--------------------------------------------------------------------------
+	| Navs
+	|--------------------------------------------------------------------------
+	|
+	| When your application is in debug mode, detailed error messages with
+	| stack traces will be shown on every error that occurs within your
+	| application. If disabled, a simple generic error page is shown.
+	|
+	*/
+
+	'nav_admin' => array(	'groups' => array(
+										'1' => Lang::get('admin.group.1'),
+										'2' => Lang::get('admin.group.2'),
+										'3' => Lang::get('admin.group.3'),
+										'4' => Lang::get('admin.group.4'),
+		),
+		),
+
+);
