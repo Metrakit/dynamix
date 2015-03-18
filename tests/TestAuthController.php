583464338c2fba67772ed8a10ec56921f054ef98
unknown <kevin.meissonnier@outlook.com>
2015-03-18 15:05:19 +0100

    Ajout du 1er test

---
+++ tests/TestAuthController.php
@@ -0,0 +1,13 @@
+<?php
+
+
+class controleur_mongo_test extends PHPUnit_Framework_TestCase
+{
+ public function testCanBeNegated()
+    {
+        // Assert
+        $this->assertEquals(1, 1);
+    }
+
+}
+?>
