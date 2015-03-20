ef60f7b403f57eaee0c8acf1a72c4b508d6df1eb
unknown <kevin.meissonnier@outlook.com>
2015-03-20 14:57:52 +0100

    nouveau test codeShip

--- tests/TestAuthController.php
+++ tests/TestAuthController.php
@@ -6,7 +6,7 @@ class controleur_mongo_test extends PHPUnit_Framework_TestCase
  public function testCanBeNegated()
     {
         // Assert
-        $this->assertEquals(1, 2);
+        $this->assertEquals(1, 1);
     }
 
 }
