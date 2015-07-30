259187c93bcd3caa45d23a8097c5c2c03d06f0f8
David Lepaux <d.lepaux@gmail.com>
2015-07-30 22:19:08 +0200

    optimize

--- theme/default/views/public/user/login.blade.php
+++ theme/default/views/public/user/login.blade.php
@@ -43,24 +43,7 @@
 
         <input type="hidden" name="remember" value="1">
 
-        @if ( Session::has('error') )
-        <div class="alert alert-danger alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            {{ Session::get('error') }}
-        </div>
-        @endif
-        @if ( Session::get('notice') )
-        <div class="alert alert-warning alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            {{ Session::get('notice') }}
-        </div>
-        @endif
-        @if ( Session::has('success') )
-        <div class="alert alert-success alert-dismissable">
-            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-            {{ Session::get('success') }}
-        </div>
-        @endif
+        @include('theme::public.session.session-message')
 
         <div class="form-group">
             <div class="col-sm-offset-2 col-sm-10">
