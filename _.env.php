bd905a6f5fb26c95ccad72c7191d8e022ef57bd7
David Lepaux <d.lepaux@gmail.com>
2015-09-23 23:31:57 +0200

    clean public folder

--- _.env.php
+++ _.env.php
@@ -20,12 +20,7 @@ return array(
 	'DB_DATABASE'         => 'dynamix',
 	'DB_USERNAME'         => 'root',
 	'DB_PASSWORD'         => 'secret',
-	
-	// Google Analytics Datas
-	'GA_CLIENT_ID'        => '',
-	'GA_SERVICE_EMAIL'    => '',
-	'GA_CERTIFICATE_PATH' => app_path() . '/config/packages/thujohn/analytics/google-analytics-api.p12',
-	'GA_USE_OBJECTS'      => true,
+
 
 
 );
